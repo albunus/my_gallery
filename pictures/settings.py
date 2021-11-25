af2b019213cf00caa01d90af9fe345bbaea7bd13
albunus <albunus.nyalita@student.moringaschool.com>
2021-11-25 23:53:38 +0300

    connecting my app to the project

--- pictures/settings.py
+++ pictures/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gallery',
+    'bootstrap4',
+    'gallery.apps.galleryConfig',
 ]
 
 MIDDLEWARE = [
