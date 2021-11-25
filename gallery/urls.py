0fcc04cb3e7b3276d924b0556661df88bf357e0c
albunus <albunus.nyalita@student.moringaschool.com>
2021-11-25 23:34:58 +0300

    creating a sample code

---
+++ gallery/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from . import views
+
+
+urlpatterns=[
+    url('^$',views.welcome,name = 'welcome'),
+]
+
--- gallery/views.py
+++ gallery/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+def welcome(request):
+    return HttpResponse('welcome to my_gallery')
