819c1ae5353635395dfdf88a4ce1d1cad569e99e
albunus <albunus.nyalita@student.moringaschool.com>
2021-11-26 23:33:26 +0300

    added my class into admin.py

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import photos, Location, Category
 
 # Register your models here.
+
+admin.site.register(photos)
+admin.site.register(Location)
+admin.site.register(Category)
