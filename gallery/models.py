846a558e20e1a958b19f78fc0e68b8848d5e45ec
albunus <albunus.nyalita@student.moringaschool.com>
2021-11-26 22:58:18 +0300

    added photos class

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+import datetime as dt
 
 # Create your models here.
+
+class photos(models.Model):
+    pic_name = models.CharField(max_length=100)
+    description = models.TextField()
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    image = CloudinaryField('image')
